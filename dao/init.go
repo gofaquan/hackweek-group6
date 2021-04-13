de6ec371ea692554141cf4d2a101ccc62753db77
kashimashino <2660796265@qq.com>
2021-04-13 09:10:08 +0800

    .env update

--- dao/init.go
+++ dao/init.go
@@ -4,14 +4,15 @@ import (
 	"github.com/kashimashino/hackweek-group6/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 var err error
 
 func InitDB() {
-	dsn := "root:MyNewPass!4@tcp(127.0.0.1:3306)/hackweek?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//dsn := "root:MyNewPass!4@tcp(127.0.0.1:3306)/hackweek?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(os.Getenv("MYSQL")), &gorm.Config{})
 	if err != nil {
 		panic("数据库连接失败")
 	}
